a7f01db1223a14f1588600afc1c2955a7d953596
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2022-05-03 17:16:39 +0300

    BSA-2: add entity package and schema.sql

---
+++ src/main/java/com/bank/spring/entity/Account.java
@@ -0,0 +1,45 @@
+package com.bank.spring.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private AccountType type;
+
+    @Column(name = "balance")
+    private BigDecimal balance;
+
+    @Column(name = "create_date")
+    private LocalDate creatingDate;
+
+    @Column(name = "status")
+    private AccountStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
